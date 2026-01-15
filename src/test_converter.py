657ede8ad225a1dc3182059e8ba4f568a3fcf00a
Akash Biswal <aakashbiswal@gmail.com>
2026-01-15 08:53:34 +0530

    feat: add converter and associated tests

---
+++ src/test_converter.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+from converter import text_node_to_html_node
+
+
+class TestConverter(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertEqual(html_node.props.get("href"), "https://www.boot.dev")
+    
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "pic.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props.get("alt"), "This is an image")
+        self.assertEqual(html_node.props.get("src"), "pic.png")
