e4ebc98d3407e4692ced846c52d77242dc33c431
Akash Biswal <aakashbiswal@gmail.com>
2026-01-16 09:16:18 +0530

    feat: add split nodes delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,6 +22,41 @@ class TestTextNode(unittest.TestCase):
         self.assertIsNotNone(node.url)
         self.assertIsNone(node2.url)
         self.assertNotEqual(node, node2)
+
+    def test_simple_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ]
+        )
+
+    def test_multiple_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word and **bold word**.", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        new_nodes_2 = new_nodes[:-1] + split_nodes_delimiter([new_nodes[-1]], "**", TextType.BOLD)
+        self.assertListEqual(
+            new_nodes_2,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word and ", TextType.TEXT),
+                TextNode("bold word", TextType.BOLD),
+                TextNode(".", TextType.TEXT),
+            ]
+        )
+
+    def test_invalid_markdown(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        self.assertRaises(
+            Exception,
+            split_nodes_delimiter([node], "`", TextType.CODE)
+        )
+
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -22,3 +22,30 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            inbetween_text = node.text.split(delimiter)
+            if len(inbetween_text) % 2 == 0 and len(inbetween_text) >= 1:
+                raise Exception("Invalid Markdown.")
+            for i, item in enumerate(inbetween_text):
+                if i % 2 == 0:
+                    new_nodes.append(
+                        TextNode(
+                            item,
+                            TextType.TEXT
+                        )
+                    ) 
+                else:
+                    new_nodes.append(
+                        TextNode(
+                            item,
+                            text_type
+                        )
+                    )
+    return new_nodes
